966a43b3b424af5eb2c4b8de19b93e498e82a7cb
Johann Arispe <johann.arispe@gmail.com>
2014-06-12 23:29:27 -0300

    test to take header photo out

--- wp-content/themes/interface/header.php
+++ wp-content/themes/interface/header.php
@@ -48,7 +48,9 @@
 		 *
 		 */
 		do_action( 'interface_links' ); ?>
-<script type="text/javascript" src=<?php echo get_template_directory_uri();?>"/js/jquery.bxslider.min.js"></script>
+
+<?php echo "<script type='text/javascript' src='.get_template_directory_uri().'/js/jquery.bxslider.min.js'></script>"?>
+
 <?php 
 		/** 
 		 * This hook is important for WordPress plugins and other many things
