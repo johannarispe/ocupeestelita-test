f3e224a3167ad739503eb1f08bd9b0a82f32ac62
Johann Arispe <johann.arispe@gmail.com>
2014-06-12 22:12:47 -0300

    test to take header photo out

--- wp-content/themes/interface/inc/structure/header-extensions.php
+++ wp-content/themes/interface/inc/structure/header-extensions.php
@@ -342,8 +342,7 @@ if (1 != $options['disable_top']) {
 <!-- Header image -->
 <?php $header_image = get_header_image();
 			if( !empty( $header_image ) ) :?>
-
-
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"> </a>
 <?php endif; ?>
 
 
